da8467470451c8ef43f9d083bf158848c19be0a8
MarceloKeijiDoi <marcelokeijidoi756@gmail.com>
2021-04-16 20:57:17 -0300

    Terceira Versao

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
+import contactsReducer from './store/contacts-reducers'
 
 import ContatosNavigator from './navegacao/ContatosNavigator'
 
 export default function App() {
+    const rootReducer = combineReducers({
+        contacts: contactsReducer
+    });
+
+    const store = createStore(rootReducer, applyMiddleware(reduxThunk));
+
     return (
-        <ContatosNavigator />
+        <Provider store={store}>
+            <ContactsNavigator />
+        </Provider>
     );
 }
